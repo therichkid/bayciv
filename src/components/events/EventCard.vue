a6b474857df1ce6e636150109da80d52b0b5a5e9
Richard Zille <therichkid@gmx.de>
2020-03-22 17:39:53 +0100

    Fix overlapping event cards on medium viewports

--- src/components/events/EventCard.vue
+++ src/components/events/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-dialog v-model="selectedOpen" scrollable max-width="600px">
     <template v-slot:activator="{ on }">
-      <v-card hover v-on="on" class="d-flex flex-column event-card" style="min-width: 250px">
+      <v-card hover v-on="on" class="d-flex flex-column event-card" style="min-width: 220px">
         <!-- Header -->
         <v-card-title class="primary white--text">
           <div v-if="event.featured">
